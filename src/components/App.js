a52c4e2e4fd1cf7fe3de5b58055833899abef4e4
kimbela winston <kimbelawin84@gmail.com>
2023-09-10 18:09:38 -0500

    hump day

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,20 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/actors" component={Actors} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
+
+
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">  {/* Note the lowercase "navbar" here */}
+      <NavLink to="/" exact>Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
+
+
+
